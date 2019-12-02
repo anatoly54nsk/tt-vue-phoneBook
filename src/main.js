f17231ebabb1f44e29e0ca256a75d06b12f6b233
Anatoly <anatoly54nsk@gmail.com>
2019-12-02 22:52:36 +0700

    [app] routing: add VueRouter based routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router";
+import MainPage from "./components/MainPage";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/', component: MainPage},
+];
+
+const router = new VueRouter({routes});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+    router,
+}).$mount('#app');
