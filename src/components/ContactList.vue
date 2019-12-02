1751f616bb92b91bc92bc78cd1e16a6513eb39ef
Anatoly <anatoly54nsk@gmail.com>
2019-12-03 00:14:20 +0700

    [components] ContactList: add ContactList

---
+++ src/components/ContactList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="contact-list">
+        <ContactCard v-for="contact in list"
+                     :contact="contact"
+                     :key="contact.id"
+                     @edit="edit"
+                     @remove="remove">
+        </ContactCard>
+    </div>
+</template>
+
+<script>
+    import ContactCard from "./ContactCard";
+
+    export default {
+        name: "ContactList",
+        components: {
+            ContactCard
+        },
+        props: {
+            list: {},
+        },
+        methods: {
+            edit(id) {
+                this.emit('edit', id);
+            },
+            remove(id) {
+                this.emit('remove', id);
+            },
+        },
+    }
+</script>
+
+<style scoped>
+.contact-list {
+    padding: 15px 0;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 25% 25% 25% 25%;
+    justify-items: center;
+    grid-row-gap: 15px;
+}
+</style>
