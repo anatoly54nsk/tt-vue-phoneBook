6669f504e950d07be11902a2db0d2429c409c6fd
Anatoly <anatoly54nsk@gmail.com>
2019-12-03 14:17:36 +0700

    [services]: ContactsApi implement pub/sub

--- src/services/ContactsApi.js
+++ src/services/ContactsApi.js
@@ -45,5 +45,20 @@ export default {
      */
     deleteContact(id) {
         return httpClient.delete(id);
+    },
+
+    listeners: {},
+
+    sub(event, listener) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+        this.listeners[event].push(listener);
+    },
+
+    pub(event, data) {
+        if (this.listeners[event]) {
+            this.listeners[event].forEach(listener => listener({event, data}));
+        }
     }
 }
